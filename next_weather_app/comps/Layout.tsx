2bfe926adb55247441dae2e997d72f56dbf13c29
i-thing <jordan_r_white@outlook.com>
2022-07-15 17:58:03 +0100

    type aded to import

--- next_weather_app/comps/Layout.tsx
+++ next_weather_app/comps/Layout.tsx
@@ -1,6 +1,6 @@
 import Navbar from "./Navbar"
 import Footer from "./Footer"
-import { PropsWithChildren } from 'react'
+import type { PropsWithChildren } from 'react'
 
 const Layout = ({ children }: PropsWithChildren) => {
     return (
